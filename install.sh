299649760b0519a4b967d02a82d86faea563969f
Willsem <happymainer@gmail.com>
2021-05-22 10:53:53 +0300

    add installation script

---
+++ install.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+ln -nfs ~/.config/nvim ./.config/nvim
+ln -nfs ~/.config/alacritty ./.config/alacritty
+ln -nfs ~/.config/kitty ./.config/kitty
+ln -nfs ~/.emacs.d ./.emacs.d
+ln -nfs ~/.yabairc ./.yabairc
+ln -nfs ~/.skhdrc ./.skhdrc
+ln -nfs ~/.zshrc ./.zshrc
