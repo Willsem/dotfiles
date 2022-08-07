5755f10058c88bbe80da901e71ca7b44459b3e3f
Willsem <happymainer@gmail.com>
2022-08-08 00:05:25 +0300

    remove old neovim config

--- .config/nvim (old)/lua/plugins/colorscheme.lua
+++
@@ -1,9 +0,0 @@
-vim.cmd[[
-if exists('+termguicolors')
-    let &t_8f = "\<Esc>[38;2;%lu;%lu;%lum"
-    let &t_8b = "\<Esc>[48;2;%lu;%lu;%lum"
-    set termguicolors
-endif
-
-colorscheme gruvbox-baby
-]]
--- .config/nvim (old)/lua/plugins/git-blame.lua
+++
@@ -1,5 +0,0 @@
-vim.cmd[[
-nnoremap <C-g> :GitBlameToggle<CR>
-]]
-
-vim.g.gitblame_enabled = 0
