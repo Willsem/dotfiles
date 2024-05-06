67d7783c081db6b4799a78ed59d56171f4b60c1d
Willsem <happymainer@gmail.com>
2024-05-06 16:09:42 +0300

    feat(term): format config

--- terminal/.wezterm.lua
+++ terminal/.wezterm.lua
@@ -1,23 +1,23 @@
-local wezterm = require('wezterm')
+local wezterm = require("wezterm")
 local config = {}
 if wezterm.config_builder then
 	config = wezterm.config_builder()
 end
 
 -- Setup colorscheme
-config.color_scheme = 'Catppuccin Frappe'
+config.color_scheme = "Catppuccin Frappe"
 
 -- Setup font
 config.font = wezterm.font_with_fallback({
-  { family = 'Cascadia Code' },
-  { family = 'nonicons', scale = 0.95 },
+	{ family = "Cascadia Code" },
+	{ family = "nonicons", scale = 0.95 },
 })
 config.font_size = 17.0
-config.line_height = 1.2
+config.line_height = 1.3
 
 -- Disable window decorations
 config.enable_tab_bar = false
-config.window_decorations = 'RESIZE'
+config.window_decorations = "RESIZE"
 
 -- Remove all paddings from the window
 config.window_padding = { left = 0, right = 0, top = 0, bottom = 0 }
