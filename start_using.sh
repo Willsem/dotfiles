b3ae854c9ed4a36b3cb371a75ffa1841c020e551
Willsem <happymainer@gmail.com>
2025-10-08 09:54:41 +0300

    feat(help): start using script update

--- start_using.sh
+++ start_using.sh
@@ -2,6 +2,15 @@
 
 pwd="$(pwd)"
 
+# setup neovim
 ln -s $pwd/nvim ~/.config
+
+# setup window manager
 ln -s $pwd/window_manager/sketchybar ~/.config
+ln -s $pwd/window_manager/.aerospace.toml ~
+
+# setup terminal
+ln -s $pwd/terminal/.zshrc ~
 ln -s $pwd/terminal/starship.toml ~/.config
+ln -s $pwd/terminal/.tmux.conf ~
+ln -s $pwd/terminal/.wezterm.lua ~
